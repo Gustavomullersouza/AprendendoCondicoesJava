c2b45d6bc1a8e1e0567106eb3beeea3edeee0dea
marcolapcx <168039947+marcolapcx@users.noreply.github.com>
2024-08-21 20:46:46 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer IdadePessoa = 20;
+        String NomePessoa = "Cleiton";
+
+        /*
+        * >  Maior
+        * >= Maior igual
+        * <  Menor
+        * <= Menor igual
+        * == é igual
+        * != é diferente
+        * equal compara Textos
+        **/
+
+        if (IdadePessoa > 21){
+
+            System.out.println("é maior que 21");
+        }else {
+            System.out.println("Não é maior");
+        }
+
     }
 }
